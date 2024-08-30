b39c51fad71c4589b21d702acf3ad2a5e7076ea6
Matias Muniz <k0v12a@proton.me>
2024-08-30 11:47:52 -0300

    Added exceptions & typing

--- movies_poo.py
+++ movies_poo.py
@@ -1,6 +1,7 @@
 import requests
 import collections
 from functools import reduce
+from typing import List, Dict, Any
 
 # Define a namedtuple called 'Movie' with fields 'name', 'imdb_rating', and 'genre'.
 Movie = collections.namedtuple('Movie', ['name', 'imdb_rating', 'genre'])
@@ -21,26 +22,38 @@ class Movies:
         self._movies = [Movie(movie['name'], movie['imdb_rating'], movie['genre'].split(", "))  for movie in self.request_movies()]
 
     @staticmethod
-    def request_movies():
+    def request_movies() -> List[Dict[str, Any]] | None:
         all_movies = []
         max_pages = 20
         page = 1
         url = "https://jsonmock.hackerrank.com/api/tvseries"
         
         while page <= max_pages:
-            all_movies.extend(requests.get(f"{url}?page={page}").json()["data"])
-            page += 1
+            try:
+                response = requests.get(f"{url}?page={page}").json()["data"]
+                all_movies.extend(response)
+                page += 1
+                
+            except requests.exceptions.RequestException as e:
+                raise SystemExit(f"Request error: {e}")
     
+            except ValueError as e:
+                raise SystemExit(f"JSON decode error: {e}")
+
         return all_movies
 
-def main():
+def main() -> Movie | None:
     movies = Movies()
     genre = "Action" 
   
     filtered_movies = [movie for movie in movies._movies if genre in movie.genre]
+    
+    if not filtered_movies:
+        return "No TV series found for the given genre."
+    
     best_movie = reduce(lambda best, current: current if (-float(current.imdb_rating), current.name) < (-float(best.imdb_rating), best.name) else best,  filtered_movies)
     
-    print(best_movie.name)
+    return best_movie
 
 if __name__ == '__main__':
     main()
